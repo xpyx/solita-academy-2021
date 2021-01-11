86f3043e009852d214aa1991d607c42bde5430af
xpyx <pystynen@gmail.com>
2021-01-12 00:08:53 +0200

    database init with people data ok

--- postgres/init.sql
+++ postgres/init.sql
@@ -1,12 +1,34 @@
-GRANT ALL PRIVILEGES ON DATABASE solita TO solita;
+DROP SCHEMA if exists public;
+CREATE SCHEMA solita;
+CREATE TABLE IF NOT EXISTS people(
+    id SERIAL,
+    name TEXT,
+    amount INTEGER,
+    createdAt TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    updatedAt TIMESTAMPTZ NOT NULL DEFAULT NOW()
+);
 
-CREATE TABLE IF NOT EXISTS solita.people ("id"   SERIAL ,
-    "name" VARCHAR(255) NOT NULL,
-    "amount" INTEGER,
-    "createdAt" TIMESTAMP WITH TIME ZONE NOT NULL,
-    "updatedAt" TIMESTAMP WITH TIME ZONE NOT NULL,
-    PRIMARY KEY ("id"));
+INSERT INTO people(name, amount)
+    VALUES
+        ('Ville', 24),
+        ('Anna', 6),
+        ('Antti', 22),
+        ('Sanna', 5),
+        ('Mikko', 19),
+        ('Minna', 5),
+        ('Timo', 18),
+        ('Satu', 5),
+        ('Tuomas', 16),
+        ('Tiina', 5),
+        ('Tero', 15),
+        ('Kati', 5),
+        ('Sami', 15),
+        ('Henna', 4),
+        ('Mika', 12),
+        ('Liisa', 4),
+        ('Janne', 12),
+        ('Paula', 4),
+        ('Petri', 11),
+        ('Suvi', 4);
 
-INSERT INTO solita.people (id, name, amount, createdAt, updatedAt)
-VALUES (1, "Sami Saari", 35, "2021-01-11T20:22:43.921Z", "2021-01-11T20:22:43.921Z")
-RETURNING "id","name","amount","createdAt","updatedAt";
+
